425208d8fd420dd0e2c1285c085d606dd5c1f195
Haoming <hchen549@wisc.edu>
2021-01-25 21:44:53 -0600

    initial project version

---
+++ js/main.js
@@ -0,0 +1,6 @@
+/* JavaScript by Haoming Chen, 2021 */
+
+var myElement = document.getElementById("myDiv")
+            myElement.addEventListener("click", () => {
+                alert("Hello World")
+            })
